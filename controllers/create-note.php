5db8b5ae060bb9626499ff25f14ab3ae8f22c8f6
dorian <mishaelwontcheu@gmail.com>
2024-02-06 09:40:18 -0400

    validation done

--- controllers/create-note.php
+++ controllers/create-note.php
@@ -13,9 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 $errors = [];
 
-     // validation
-     if (strlen($_POST['body']) == 0) { 
-        $erros['body'] = 'A body is required';
+     // validation qu'on entre quelque chose dans notre body
+     if (strlen($_POST['body']) === 0) { 
+        $errors['body'] = 'A body is required';
+     }
+
+     // validation de la taille du charactere 
+     if (strlen($_POST['body']) > 1000) { 
+        $errors['body'] = 'The body can not be more than 1000 characters';
      }
 
      if(empty($errors)) {
