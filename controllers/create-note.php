9e8b6db78020f4dfa1c316a00381c69e96158b60
dorian <mishaelwontcheu@gmail.com>
2024-02-05 22:46:35 -0400

    to fix: validation form

--- controllers/create-note.php
+++ controllers/create-note.php
@@ -10,10 +10,21 @@ $heading = 'Create Note';
 //dd($_SERVER);
 // effetue un envoie de donnees vers ma db, POST 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-     $db->query('INSERT INTO notes(body, users_id) VALUES(:body, :users_id)', [
-        'body'=> $_POST['body'],
-        'users_id'=> 2
-     ]);
+
+$errors = [];
+
+     // validation
+     if (strlen($_POST['body']) == 0) { 
+        $erros['body'] = 'A body is required';
+     }
+
+     if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, users_id) VALUES(:body, :users_id)', [
+            'body'=> $_POST['body'],
+            'users_id'=> 2
+         ]);
+     }
+
 }
 
 
